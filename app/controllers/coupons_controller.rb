85e23a2b049dfab5aaa9f9ff4ba9a4dffe775c98
Joy Lin <vvlnote@hotmail.com>
2019-05-21 17:27:59 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,7 +15,7 @@ class CouponsController < ApplicationController
   end
   
   def create
-    binding.pry
+    puts "#{params}"
     coupon = Coupon.new
     coupon.coupon_code = params[:coupon_code]
     coupon.store = params[:store]
